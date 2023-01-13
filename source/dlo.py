d3f5b462dbc13e598ca61d2e67e21ad05b18fe5f
Bence Drigan <g.drigan@students.htl-leonding.ac.at>
2023-01-13 08:40:16 +0100

    Inserting JSON Handler in DLO.py

--- source/dlo.py
+++ source/dlo.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
 import sys
+import json
 
 # Set the preset to default
 preset = "default"
 
 # Set the directory where the files are located
-directory = "C:\\Users\\" + os.getenv("username") + "\\AppData\\Local\\DLO"
+registryDirectory = "C:\\Users\\" + os.getenv("username") + "\\AppData\\Local\\DLO"
+presetListFilename = "C:\\Users\\" + os.getenv("username") + "\\AppData\\Local\\DLO\\Presets\\PresetList.json"
 
 def main():
     while True:
@@ -20,7 +22,7 @@ def main():
         print(" [5]: Delete Preset")
         print(" [6]: Exit")
         print("=========================================")
-        print(" Current Preset:", preset)
+        print(" Current Preset:", presetName)
         print("=========================================")
 
         # Get the user's input
@@ -42,20 +44,41 @@ def main():
             print("[DLO] ERROR: Unknown command")
 
 def create_preset():
-    global preset
-    preset = input("Preset name: ")
-    # Export the registry key to the Presets directory
-    subprocess.call(f"reg export HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\Shell\\Bags\\1\\Desktop {os.path.join(directory, 'Presets', preset)}.reg", shell=True)
+    global presetName
+    presetName = input("Preset name: ")
+    # Export the registry key to the registryDirectory
+    subprocess.call(f"reg export HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\Shell\\Bags\\1\\Desktop {os.path.join(registryDirectory, 'Presets', preset)}.reg", shell=True)
+    
+    # 1 JSON File laden
+
+    f = open(f"{presetListFilename}", "r")
+    presetList = json.loads(f.read())
+
+    # 2 Schauen, ob der Eintrag schon drin ist
+
+    foundPresetInList = False
+    for actPreset in presetList['presets']:
+        if actPreset['name'] == presetName:
+            foundPresetInList = True
+            break
+
+    # 3 Wenn schon drin ist, Fehler zurückgeben, ansonsten Eintrag hinzufügen
+
+    if foundPresetInList:
+        print("[DLO]: A preset with that name already exists.")
+    else: 
+        print("[DLO]: Adding entry... ")
+    
     input("Press enter to continue...")
     main()
 
 def change_preset():
-    global preset
-    # Change to the Presets directory
-    os.chdir(os.path.join(directory, "Presets"))
-    # List the files in the directory
+    global presetName
+    # Change to the registry directory
+    os.chdir(os.path.join(registryDirectory, "Presets"))
+    # List the files in the registry directory
     print(os.listdir())
-    preset = input("Preset name: ")
+    presetName = input("Preset name: ")
     input("Press enter to continue...")
     main()
 
@@ -63,24 +86,24 @@ def save_preset():
     print("[DLO] -> Please manually refresh the desktop...")
     input("Press enter to continue...")
     # Delete the existing preset file
-    os.remove(os.path.join(directory, "Presets", preset + ".reg"))
-    # Export the registry key to the Presets directory
-    subprocess.call(f"reg export HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\Shell\\Bags\\1\\Desktop {os.path.join(directory, 'Presets', preset)}.reg", shell=True)
+    os.remove(os.path.join(registryDirectory, "Presets", presetName + ".reg"))
+    # Export the registry key to the registry directory
+    subprocess.call(f"reg export HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\Shell\\Bags\\1\\Desktop {os.path.join(registryDirectory, 'Presets', preset)}.reg", shell=True)
 
 
 def load_preset():
-    global preset
-    presetFileName = os.path.join(directory, "Presets", preset + ".reg")
+    global presetName
+    presetFileName = os.path.join(registryDirectory, "Presets", presetName + ".reg")
     subprocess.call(['reg', 'import', presetFileName])
     os.system("taskkill /im explorer.exe /F")
     subprocess.Popen(['explorer'], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
     return
 
 def delete_preset():
-    global preset
-    # Change to the Presets directory
-    os.chdir(os.path.join(directory, "Presets"))
-    # List the files in the directory
+    global presetName
+    # Change to the registry directory
+    os.chdir(os.path.join(registryDirectory, "Presets"))
+    # List the files in the registry directory
     print(os.listdir())
     presetToDelete = input("Preset name: ")
     os.remove(presetToDelete + ".reg")
@@ -88,10 +111,10 @@ def delete_preset():
     return
 
 # Check if the directories exist
-if os.path.exists(directory) and os.path.exists(os.path.join(directory, "Presets")) and os.path.exists(os.path.join(directory, "Repository")):
+if os.path.exists(registryDirectory) and os.path.exists(os.path.join(registryDirectory, "Presets")) and os.path.exists(os.path.join(registryDirectory, "Repository")):
     # Call the main function
     main()
 else:
     # Create the directories
-    os.makedirs(os.path.join(directory, "Presets"))
-    os.makedirs(os.path.join(directory, "Repository"))
+    os.makedirs(os.path.join(registryDirectory, "Presets"))
+    os.makedirs(os.path.join(registryDirectory, "Repository"))
