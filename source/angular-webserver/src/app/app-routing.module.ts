7f5293246fe6524de40c69d6def03aa723884965
Patrik Kocsis <kocsispatrikistvan@gmail.com>
2024-01-30 10:13:37 +0100

    Created a seperate SSO page for the login service

--- source/angular-webserver/src/app/app-routing.module.ts
+++ source/angular-webserver/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SsoComponent } from './sso/sso.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/sso', pathMatch: 'full'},
+  {path: 'sso', component: SsoComponent},
+  //{path: 'home', component: AppComponent},
+  //{path: '', redirectTo: '/home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
