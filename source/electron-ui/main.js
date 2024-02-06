524214f2896e0c179ebd984e11940c567a6cde7c
Bence Drigan <g.drigan@students.htl-leonding.ac.at>
2024-02-06 10:02:28 +0100

    dev/prod api start
    - the API server can now be started in both dev und production mode

--- source/electron-ui/main.js
+++ source/electron-ui/main.js
@@ -1,7 +1,9 @@
 const { app, BrowserWindow, utilityProcess } = require('electron');
 const { spawn, exec, execSync } = require('node:child_process');
+const path = require('node:path');
 
 let win = null;
+let apiServerPath = "";
 
 const createWindow = () => {
     win = new BrowserWindow({
@@ -28,8 +30,22 @@ app.whenReady().then(() => {
  * Checks the PIDs of the API Server and kills them when the window is closed.
  */
 const startAPI = () => {  
+
+    var env = process.env.NODE_ENV || 'production';
+
+    if (env === 'production') {
+        apiServerPath = path.join(__dirname, '..', 'assets', 'api-server', 'dist');
+    } else {
+        apiServerPath = path.join(__dirname, 'assets', 'api-server', 'dist');
+    }
+
+    // I assume execSync works with absolute paths, since the built version of the application is throwing error not finding the assets folder...
+    // prepend spaces in the path to avoid errors
+    apiServerPath = `"${apiServerPath}"`;
+
+
     // start API server
-    exec('assets\\api-server\\dist\\dhapi.exe', function (err, data) {
+    exec(path.join(apiServerPath, 'dhapi.exe'), function (err, data) {
         if (err) {
             console.error(`[DHAPI] > Error: ${err.message}`);
             return;
@@ -38,9 +54,9 @@ const startAPI = () => {
         }
     })
 
-    // run the executable that kills the API server)
+    // run the executable that kills the API server
     win.on('close', () => {
-        execSync('assets\\api-server\\dist\\terminate_api.exe', function (err, data) {
+        execSync(path.join(apiServerPath, 'terminate_api.exe'), function (err, data) {
             if (err) {
                 console.error(`[DHAPI] > Error: ${err.message}`);
                 return;
