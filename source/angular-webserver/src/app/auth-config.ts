93a54d4c55d23afd985743641b7caf5e52378941
Bence Drigan <g.drigan@students.htl-leonding.ac.at>
2024-02-16 11:11:55 +0100

    adjustments for kubernetes

--- source/angular-webserver/src/app/auth-config.ts
+++ source/angular-webserver/src/app/auth-config.ts
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: '30a4b95d-3e03-4b2a-ae30-7acdb2a3af21',
       authority: 'https://login.microsoftonline.com/91fc072c-edef-4f97-bdc5-cfb67718ae3a',
-      redirectUri: 'http://localhost:4200',
+      redirectUri: 'https://student.cloud.htl-leonding.ac.at/g.drigan/angular-webserver/',
     },
     cache: {
       cacheLocation: 'localStorage',
